901df9fce6361a2e61027c5516048a7916ee52c8
Sobothty <supercrisz24@gmail.com>
2025-06-13 15:14:42 +0700

    o sl ke

---
+++ src/components/Payment/PaymentForm.tsx
@@ -0,0 +1,53 @@
+
+// CheckoutForm.tsx
+import React, { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import axios from 'axios';
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [amount, setAmount] = useState(100); // USD
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) return;
+
+    const cardElement = elements.getElement(CardElement);
+
+    const { token, error } = await stripe.createToken(cardElement!);
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    // Send token to backend
+    const response = await axios.post(`https://api-hotel-production-ee3e.up.railway.app/api/stripe/charge`, {
+      amount,
+      currency: 'usd',
+      token: token.id,
+    });
+
+    alert(response.data.message);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Amount (USD):</label>
+      <input type="number"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        required
+      />
+      <label>Card Details:</label>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>Pay Now</button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
+
